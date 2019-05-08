2c671a4ec434571973e59b559946d061bd00127c
Usuario <Usuario@A14-M01>
2019-05-08 11:46:17 -0300

    Empezando

--- ConsultasServidor/web/programa.js
+++ ConsultasServidor/web/programa.js
@@ -8,8 +8,9 @@ class Sube{
         console.log("Aca se hace la consulta");
         //1. Si tenes saldo o no.
         //2. Mostrar saldo
-        fetch
-    }
+        fetch("ConsultaDeSaldo",{
+            method: 'GET'});        }
+    
 }
 
 Sube.main();
